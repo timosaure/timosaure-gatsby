e2f6b05d21c2bb5b46c1947f568982f76618f03b
Timo Saure <sauretimo@googlemail.com>
2021-01-12 18:53:30 +0100

    Add navigation bar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,49 +1,52 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { Link } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import "./layout.scss"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
+      <header>
+        <nav
+          className="navbar is-white is-fixed-top"
+          role="navigation"
+          aria-label="main navigation"
         >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+          <div className="navbar-brand">
+            <Link className="navbar-item" to="/">
+              <span className="logo">TIMO</span>
+            </Link>
+
+            <a
+              role="button"
+              className="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="main-navbar-items"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div id="main-navbar-items" className="navbar-menu">
+            <div className="navbar-start">
+              <Link className="navbar-item" to="/about">
+                About
+              </Link>
+              <Link className="navbar-item" to="/projects">
+                Projects
+              </Link>
+              <Link className="navbar-item" to="/skills">
+                Skills
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main>{children}</main>
     </>
   )
 }
