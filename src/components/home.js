9f0018d065b3cf162b1cb5aea8707dc5cea584c5
Timo Saure <sauretimo@googlemail.com>
2021-01-12 19:46:30 +0100

    Add basic home page

---
+++ src/components/home.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+import './home.scss'
+
+const Home = () => (
+  <>
+    <section class="hero is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="main-title is-uppercase">Timo Saure</h1>
+          <h2 class="main-subtitle">Software Engineer</h2>
+          <h2 class="main-subtitle">Full Stack Developer</h2>
+          <h2 class="main-subtitle">Data Engineer</h2>
+        </div>
+      </div>
+    </section>
+    <section class="hero is-info is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">Timo Saure</h1>
+          <h2 class="subtitle">Software Engineer</h2>
+        </div>
+      </div>
+    </section>
+  </>
+)
+
+export default Home
