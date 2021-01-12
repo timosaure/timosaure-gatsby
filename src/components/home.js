cac43a49158d96e2b699b7c017dcbbd07deafaef
Timo Saure <sauretimo@googlemail.com>
2021-01-12 21:12:38 +0100

    Fix home.js not using react className syntax

--- src/components/home.js
+++ src/components/home.js
@@ -4,21 +4,21 @@ import './home.scss'
 
 const Home = () => (
   <>
-    <section class="hero is-fullheight">
-      <div class="hero-body">
-        <div class="container has-text-centered">
-          <h1 class="main-title is-uppercase">Timo Saure</h1>
-          <h2 class="main-subtitle">Software Engineer</h2>
-          <h2 class="main-subtitle">Full Stack Developer</h2>
-          <h2 class="main-subtitle">Data Engineer</h2>
+    <section className="hero is-fullheight">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="main-title is-uppercase">Timo Saure</h1>
+          <h2 className="main-subtitle">Software Engineer</h2>
+          <h2 className="main-subtitle">Full Stack Developer</h2>
+          <h2 className="main-subtitle">Data Engineer</h2>
         </div>
       </div>
     </section>
-    <section class="hero is-info is-fullheight">
-      <div class="hero-body">
-        <div class="container has-text-centered">
-          <h1 class="title">Timo Saure</h1>
-          <h2 class="subtitle">Software Engineer</h2>
+    <section className="hero is-info is-fullheight">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title">Timo Saure</h1>
+          <h2 className="subtitle">Software Engineer</h2>
         </div>
       </div>
     </section>
