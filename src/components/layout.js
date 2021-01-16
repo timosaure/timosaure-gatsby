c46e546c73d0f94afc696ac3d1deeac81adaae62
Timo Saure <sauretimo@googlemail.com>
2021-01-16 01:24:57 +0100

    Fix footer flying above bottom on small pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main className="main-content">{children}</main>
       <Footer />
     </>
   )
