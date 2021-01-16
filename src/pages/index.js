f6fca282610afb125971152b020800c8d1bcfb27
Timo Saure <sauretimo@googlemail.com>
2021-01-16 19:54:58 +0100

    Clean up the structure, remove starter files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
-import Home from "../components/home"
+import Home from "../components/home/home"
 
-import Layout from "../components/layout"
+import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 import "../scss/styles.scss"
 
