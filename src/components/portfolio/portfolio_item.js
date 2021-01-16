41ba5fd2d1371bf052da8fabba8d1e0f76295c78
Timo Saure <sauretimo@googlemail.com>
2021-01-16 18:10:55 +0100

    Improve portfolio logo size on mobile

--- src/components/portfolio/portfolio_item.js
+++ src/components/portfolio/portfolio_item.js
@@ -7,10 +7,10 @@ import BulletPoints from "./bullet_points"
 const PortfolioItem = ({ item }) => {
   return (
     <div className="columns portfolio-item-row">
-      <div className="column is-one-quarter pr-6">
+      <div className="column is-3 is-6-mobile">
         <Img fluid={item.logo.childImageSharp.fluid} />
       </div>
-      <div className="column is-three-quarters">
+      <div className="column is-8 is-offset-1">
         <h2 className="title is-5">{item.position}</h2>
         <h3 className="subtitle is-6 mb-1">
           {item.company} <span className="mx-1">|</span> {item.timeframe}
