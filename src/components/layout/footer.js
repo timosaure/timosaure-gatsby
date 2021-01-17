045f94c181954e0f65ccb62b7b7e5c4904ceaa96
Timo Saure <sauretimo@googlemail.com>
2021-01-17 20:25:52 +0100

    Change github icon to be easier to identify

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -3,13 +3,13 @@ import React from "react"
 import SocialLink from "./social_link"
 
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
-import { faGithubAlt, faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="content has-text-centered">
-        <SocialLink icon={faGithubAlt} href="https://github.com/timosaure" />
+        <SocialLink icon={faGithub} href="https://github.com/timosaure" />
         <SocialLink
           icon={faLinkedinIn}
           href="https://www.linkedin.com/in/timosaure"
