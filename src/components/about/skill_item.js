3080e20cd1daf69f9105dd6407599c2d5f79c507
Timo Saure <sauretimo@googlemail.com>
2021-01-17 13:08:39 +0100

    Adjust formatting of the skill items a little bit

--- src/components/about/skill_item.js
+++ src/components/about/skill_item.js
@@ -4,13 +4,12 @@ import Img from "gatsby-image"
 import Image from "../image"
 
 const SkillItem = ({ skill }) => {
-  console.log(skill)
   return (
     <div className="columns portfolio-item-row">
-      <div className="column is-1 is-2-mobile">
+      <div className="column is-1 is-offset-1 is-2-mobile">
         <Image image={skill.logo} />
       </div>
-      <div className="column is-8">
+      <div className="column is-9">
         <p>{skill.description}</p>
       </div>
     </div>
