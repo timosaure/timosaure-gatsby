9b642e1273cf97a86f8a1ff88793f1dc57fc2cf8
Timo Saure <sauretimo@googlemail.com>
2021-01-17 14:50:54 +0100

    Fix layout of skill on mobile

--- src/components/about/skill_item.js
+++ src/components/about/skill_item.js
@@ -5,11 +5,11 @@ import Image from "../image"
 
 const SkillItem = ({ skill }) => {
   return (
-    <div className="columns portfolio-item-row">
-      <div className="column is-1 is-offset-1 is-2-mobile">
+    <div className="columns is-mobile portfolio-item-row">
+      <div className="column  is-2-mobile is-offset-0-mobile is-1-tablet is-offset-1-tablet">
         <Image image={skill.logo} />
       </div>
-      <div className="column is-9">
+      <div className="column is-9-tablet is-10-mobile">
         <p>{skill.description}</p>
       </div>
     </div>
