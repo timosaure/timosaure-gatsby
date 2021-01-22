2a3d393087046160a8bee168b391019f07d94beb
Timo Saure <sauretimo@googlemail.com>
2021-01-23 00:14:38 +0100

    Move contact page to directory

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 
-import Layout from "../components/layout/layout"
-import SEO from "../components/seo"
-import ContactForm from "../components/contact_form/contact_form"
+import Layout from "../../components/layout/layout"
+import SEO from "../../components/seo"
+import ContactForm from "../../components/contact_form/contact_form"
 
 const IndexPage = () => (
   <Layout>
