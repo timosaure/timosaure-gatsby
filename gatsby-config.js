64c925a9c9a121d8d196617939b260c769a05612
Timo Saure <sauretimo@googlemail.com>
2021-01-25 00:05:07 +0100

    Add support for i18n to localize the website

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-react-i18next`,
+      options: {
+        path: `${__dirname}/src/data/`,
+        languages: [`en`, `de`],
+        defaultLanguage: `de`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
