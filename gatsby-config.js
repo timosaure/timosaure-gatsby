2eeb2e4a8ba2960e73e4c45625f5ee035f675f7e
Timo Saure <sauretimo@googlemail.com>
2021-01-31 20:32:24 +0100

    Add sitemap generation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
 module.exports = {
   siteMetadata: {
+    siteUrl: `https://www.timosaure.com`,
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
@@ -44,6 +45,7 @@ module.exports = {
         defaultLanguage: `de`,
       },
     },
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
