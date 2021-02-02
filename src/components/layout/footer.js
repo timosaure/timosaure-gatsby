029ad9eb258d19b58502a542a7519d88f6b2e813
Timo Saure <sauretimo@googlemail.com>
2021-02-02 19:55:02 +0100

    Add detail text to footer

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -4,8 +4,11 @@ import SocialLink from "./social_link"
 
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
+import { useTranslation } from "react-i18next"
 
 const Footer = () => {
+  const { t } = useTranslation()
+
   return (
     <footer className="footer">
       <div className="content is-flex is-flex-direction-column is-align-items-center">
@@ -18,8 +21,9 @@ const Footer = () => {
           <SocialLink icon={faEnvelope} href="/contact" />
         </div>
         <p className="has-text-white mt-5">
-          Copyright Ⓒ <span>{new Date().getFullYear()}</span> Timo Saure
+          {"Copyright Ⓒ " + new Date().getFullYear() + " Timo Saure"}
         </p>
+        <p className="has-text-white">{t("footer_detail")}</p>
       </div>
     </footer>
   )
